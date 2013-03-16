e929f8d770d25ad4820d40d9e6c068b9d67fe5ba
Alexander Kulakov <a.kulakov@mail.ru>
2013-03-16 09:35:47 -0400

    * Updated header

--- gevent_fastcgi/adapters/django/management/commands/run_gevent_fastcgi.py
+++ gevent_fastcgi/adapters/django/management/commands/run_gevent_fastcgi.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2011-2013, Alexander Kulakov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in
+#    all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#    THE SOFTWARE.
+
 import os
 import re
 from optparse import make_option
@@ -11,8 +31,6 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--maxconns', type='int', dest='max_conns', default=1024,
             metavar='MAX_CONNS', help='Maximum simulteneous connections (default %default)'),
-        make_option('--maxreqs', type='int', dest='max_reqs', default=1024,
-            metavar='MAX_REQS', help='Maximum active requests (default %default)'),
         make_option('--daemon', action='store_true', dest='daemonize', default=False,
             help='Become a daemon'),
         make_option('--workdir', dest='our_home_dir', default='.', metavar='WORKDIR',
