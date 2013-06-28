84ef7e708018917382abc2aaa0112aee67491725
Alexander Kulakov <a.kulakov@mail.ru>
2013-06-28 11:23:26 -0400

    Fix Django adapter

--- gevent_fastcgi/adapters/django/management/commands/run_gevent_fastcgi.py
+++ gevent_fastcgi/adapters/django/management/commands/run_gevent_fastcgi.py
@@ -18,25 +18,37 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import os
-import re
 from optparse import make_option
 from django.core.management import BaseCommand, CommandError
 
-__all__ = ['GeventFastCGI']
-
 
 class Command(BaseCommand):
-    args = '<host:port>'
+    args = '<host>:<port> | <socket file>'
     help = 'Start gevent-fastcgi server'
     option_list = BaseCommand.option_list + (
-        make_option('--maxconns', type='int', dest='max_conns', default=1024,
+        make_option('--max-conns', type='int', dest='max_conns', default=1024,
                     metavar='MAX_CONNS',
                     help='Maximum simulteneous connections (default %default)',
                     ),
+        make_option('--buffer-size', type='int', dest='buffer_size',
+                    default=4096, metavar='BUFFER_SIZE',
+                    help='Read buffer size (default %default)',
+                    ),
+        make_option('--num-workers', type='int', dest='num_workers', default=1,
+                    metavar='NUM_WORKERS',
+                    help='Number of worker processes (default %default)',
+                    ),
+        make_option('--backlog', type='int', dest='backlog',
+                    metavar='LISTEN_BACKLOG',
+                    help='Listen backlog (default %default)',
+                    ),
+        make_option('--socket-mode', type='int', dest='socket_mode',
+                    metavar='SOCKET_MODE',
+                    help='Socket file mode',
+                    ),
         make_option('--daemon', action='store_true', dest='daemonize',
                     default=False, help='Become a daemon'),
-        make_option('--workdir', dest='our_home_dir', default='.',
+        make_option('--work-dir', dest='our_home_dir', default='.',
                     metavar='WORKDIR',
                     help='Chande dir in daemon mode (default %default)'),
         make_option('--stdout', dest='out_log', metavar='STDOUT',
@@ -49,29 +61,32 @@ class Command(BaseCommand):
     )
 
     def handle(self, *args, **options):
-        from os.path import abspath, dirname, isdir
+        from os.path import dirname, isdir
         from gevent_fastcgi.server import FastCGIServer
         from gevent_fastcgi.wsgi import WSGIRequestHandler
         from django.core.handlers.wsgi import WSGIHandler
 
         if not args:
-            raise CommandError('bind address is not specified')
+            raise CommandError('Please specify binding address')
 
         if len(args) > 1:
-            raise CommandError('unexpected arguments: %s', ' '.join(args[1:]))
+            raise CommandError('Unexpected arguments: %s' % ' '.join(args[1:]))
+
+        bind_address = args[0]
 
         try:
-            host, port = args[0].split(':', 1)
+            host, port = bind_address.split(':', 1)
         except ValueError:
-            address = abspath(args[0])
-            if not isdir(dirname(address)):
+            socket_dir = dirname(bind_address)
+            if not isdir(socket_dir):
                 raise CommandError(
-                    'directory %s does not exist', dirname(address))
+                    'Please create directory for socket file first %r' %
+                    dirname(socket_dir))
         else:
             try:
-                address = (host, int(port))
+                bind_address = (host, int(port))
             except ValueError:
-                raise CommandError('port must be an integer value')
+                raise CommandError('Invalid binding address %r' % bind_address)
 
         if options['daemonize']:
             from django.utils.daemonize import become_daemon
@@ -81,11 +96,12 @@ class Command(BaseCommand):
                     'our_home_dir', 'out_log', 'err_log', 'umask'))
             become_daemon(**daemon_opts)
 
-        for name in ('num_workers', 'max_conns', 'buffer_size'):
-            if name in options:
-                options[name] = int(options[name])
+        kwargs = dict((
+            (name, value) for name, value in options.iteritems() if name in (
+                'num_workers', 'max_conns', 'buffer_size', 'backlog',
+                'socket_mode')))
 
         app = WSGIHandler()
         request_handler = WSGIRequestHandler(app)
-        server = FastCGIServer(address, request_handler, **options)
+        server = FastCGIServer(bind_address, request_handler, **kwargs)
         server.serve_forever()
