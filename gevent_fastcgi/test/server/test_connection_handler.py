f679004a2d5be3dfdeb53edfafc1c8098aacd8f8
Alexander Kulakov <a.kulakov@mail.ru>
2013-07-01 13:57:36 -0400

    Move TestConnectionHandler out of ConnectionHandlerTests

--- gevent_fastcgi/test/server/test_connection_handler.py
+++ gevent_fastcgi/test/server/test_connection_handler.py
@@ -15,6 +15,53 @@ from ...const import (
     FCGI_NULL_REQUEST_ID,
 )
 
+try:
+    from ...server import ConnectionHandler, ServerConnection
+except ImportError:
+    logger.exception('Failed to import classes')
+
+    class TestConnectionHandler(object):
+        pass
+else:
+    class TestConnectionHandler(ConnectionHandler):
+        """ ConnectionHandler that intersepts send_record calls.
+
+        It also has some handy methods used by tests
+        """
+        def __init__(self, conn=None, role=FCGI_RESPONDER, capabilities={},
+                     request_handler=None):
+            from ..utils import MockSocket
+
+            if conn is None:
+                conn = ServerConnection(MockSocket())
+            if request_handler is None:
+                request_handler = self.default_request_handler
+            ConnectionHandler.__init__(
+                self, conn, role, capabilities, request_handler)
+            self.records_sent = []
+
+        def send_record(self, record_type, content='',
+                        request_id=FCGI_NULL_REQUEST_ID):
+            self.records_sent.append(Record(record_type, content,
+                                            request_id))
+
+        def begin_request(self, request_id=None, role=None, flags=0):
+            from ...const import FCGI_BEGIN_REQUEST
+
+            if request_id is None:
+                request_id = random_request_id()
+            if role is None:
+                role = self.role
+            record = Record(FCGI_BEGIN_REQUEST,
+                            pack_begin_request(role, flags), request_id)
+            self.handle_begin_request_record(record)
+
+            return self.requests.get(request_id)
+
+        @staticmethod
+        def default_request_handler(request):
+            pass
+
 
 def random_request_id():
     return random.randint(1, 65535)
@@ -32,7 +79,7 @@ class ConnectionHandlerTests(unittest.TestCase):
             (FCGI_MAX_REQS, ''),
             (FCGI_MPXS_CONNS, ''),
         ))
-        handler = self.create_handler()
+        handler = TestConnectionHandler()
 
         handler.send_record(record_type, content, FCGI_NULL_REQUEST_ID)
 
@@ -43,7 +90,7 @@ class ConnectionHandlerTests(unittest.TestCase):
         assert record.request_id == FCGI_NULL_REQUEST_ID
 
     def test_begin_request(self):
-        handler = self.create_handler()
+        handler = TestConnectionHandler()
         request = handler.begin_request()
 
         assert len(handler.requests) == 1
@@ -53,7 +100,7 @@ class ConnectionHandlerTests(unittest.TestCase):
     def test_begin_filter_request(self):
         from ...const import FCGI_FILTER
 
-        handler = self.create_handler(role=FCGI_FILTER)
+        handler = TestConnectionHandler(role=FCGI_FILTER)
         request = handler.begin_request()
 
         assert isinstance(request.data, InputStream)
@@ -61,7 +108,7 @@ class ConnectionHandlerTests(unittest.TestCase):
     def test_begin_request_keep_open(self):
         from ...const import FCGI_ABORT_REQUEST, FCGI_KEEP_CONN
 
-        handler = self.create_handler()
+        handler = TestConnectionHandler()
         request = handler.begin_request(flags=FCGI_KEEP_CONN)
         handler.handle_abort_request_record(
             Record(FCGI_ABORT_REQUEST, '', request.id), request)
@@ -79,7 +126,7 @@ class ConnectionHandlerTests(unittest.TestCase):
             FCGI_UNKNOWN_ROLE,
         )
 
-        handler = self.create_handler(role=FCGI_RESPONDER)
+        handler = TestConnectionHandler(role=FCGI_RESPONDER)
         request = handler.begin_request(role=FCGI_AUTHORIZER)
 
         assert request is None
@@ -95,7 +142,7 @@ class ConnectionHandlerTests(unittest.TestCase):
         from ...const import FCGI_PARAMS
 
         env = os.environ.copy()
-        handler = self.create_handler()
+        handler = TestConnectionHandler()
         request = handler.begin_request()
         record = Record(FCGI_PARAMS, pack_pairs(env), request.id)
 
@@ -108,7 +155,7 @@ class ConnectionHandlerTests(unittest.TestCase):
     def test_abort_request(self):
         from ...const import FCGI_ABORT_REQUEST, FCGI_END_REQUEST
 
-        handler = self.create_handler()
+        handler = TestConnectionHandler()
         request = handler.begin_request()
         record = Record(FCGI_ABORT_REQUEST, '', request.id)
 
@@ -131,8 +178,8 @@ class ConnectionHandlerTests(unittest.TestCase):
             lock.set()
             sleep(3)
 
-        handler = self.create_handler(role=FCGI_RESPONDER,
-                                      request_handler=handle_request)
+        handler = TestConnectionHandler(role=FCGI_RESPONDER,
+                                        request_handler=handle_request)
         request = handler.begin_request()
         # next records should spawn request handler
         handler.handle_params_record(
@@ -162,7 +209,7 @@ class ConnectionHandlerTests(unittest.TestCase):
             FCGI_MAX_REQS: 1,
             FCGI_MPXS_CONNS: 0,
         }
-        handler = self.create_handler(capabilities=server_caps)
+        handler = TestConnectionHandler(capabilities=server_caps)
         request_caps = dict.fromkeys(server_caps, '')
         record = Record(FCGI_GET_VALUES, pack_pairs(request_caps), 0)
 
@@ -175,49 +222,3 @@ class ConnectionHandlerTests(unittest.TestCase):
         for cap in server_caps:
             assert cap in response_caps, repr(response_caps)
             assert isinstance(response_caps[cap], str)
-
-    def create_handler(self, conn=None, role=FCGI_RESPONDER, capabilities={},
-                       request_handler=None):
-        from ...server import ConnectionHandler
-
-        class TestConnectionHandler(ConnectionHandler):
-            """ ConnectionHandler that intersepts send_record calls.
-
-            It also has some handy methods used by tests
-            """
-            def __init__(self, conn=None, role=FCGI_RESPONDER, capabilities={},
-                         request_handler=None):
-                from ...server import ServerConnection
-                from ..utils import MockSocket
-
-                if conn is None:
-                    conn = ServerConnection(MockSocket())
-                if request_handler is None:
-                    request_handler = self.default_request_handler
-                ConnectionHandler.__init__(
-                    self, conn, role, capabilities, request_handler)
-                self.records_sent = []
-
-            def send_record(self, record_type, content='',
-                            request_id=FCGI_NULL_REQUEST_ID):
-                self.records_sent.append(Record(record_type, content,
-                                                request_id))
-
-            def begin_request(self, request_id=None, role=None, flags=0):
-                from ...const import FCGI_BEGIN_REQUEST
-
-                if request_id is None:
-                    request_id = random_request_id()
-                if role is None:
-                    role = self.role
-                record = Record(FCGI_BEGIN_REQUEST,
-                                pack_begin_request(role, flags), request_id)
-                self.handle_begin_request_record(record)
-
-                return self.requests.get(request_id)
-
-            @staticmethod
-            def default_request_handler(request):
-                pass
-
-        return TestConnectionHandler(conn, role, capabilities, request_handler)
