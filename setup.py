8210e81693ad2de109489ad2536d05c36999500c
Alexander Kulakov <a.kulakov@mail.ru>
2014-01-16 18:41:52 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if '__pypy__' not in sys.builtin_module_names:
 
 setup(
     name='gevent-fastcgi',
-    version='0.1.25.1dev',
+    version='0.1.25.2dev',
     description='''FastCGI/WSGI client and server implemented using gevent
     library''',
     long_description='''
